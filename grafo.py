4cfe1261b1af42d3a12435a24b835a256366f776
Felipe Chatalov <73392575+felipechatalov@users.noreply.github.com>
2022-04-12 00:42:54 -0300

    Add files via upload

--- grafo.py
+++ grafo.py
@@ -3,9 +3,25 @@ class Vertice():
         self.id = id
         self.conexoes = []
         self.visited = False
+        
+        # bellmanFord
+        self.bf_pai = None
+        self.bf_distancia = float('inf')
+
+        # dijkstra
+        self.dj_pai = None
+        self.dj_distancia = float('inf')
+
+    def __str__(self):
+        return f'id: {self.id}'
 
     def addAresta(self, dst, weight):
         self.conexoes.append(Aresta(dst, weight))
+
+    def getArestaFromVertice(self, dst):
+        for aresta in self.conexoes:
+            if aresta.dst == dst:
+                return aresta
 
 
 class Aresta():
@@ -22,6 +38,7 @@ def resetVisitedNodes(g):
 
 
 def bfs(g, pai):
+    resetVisitedNodes(g)
 
     q = []
     seq = []
@@ -51,12 +68,11 @@ def bfs(g, pai):
     for v in seq:
         print(v.id, end=" ") 
     print()
-
-    resetVisitedNodes(g)
     return seq
 
 def dfs(g, pai):
-
+    resetVisitedNodes(g)
+    
     q = []
     seq = []
 
@@ -85,17 +101,82 @@ def dfs(g, pai):
     for v in seq:
         print(v.id, end=" ") 
     print()
-
-    resetVisitedNodes(g)
     return seq
 
-       
+
+def relax(p, f):
+    # print(f'\n relaxando {p.id} -> {f.id}')
+    if p.bf_distancia + p.getArestaFromVertice(f).w < f.bf_distancia:
+        f.bf_distancia = p.bf_distancia + p.getArestaFromVertice(f).w
+        # nao eh usado no algoritmo mas pode se usado para retorna do 
+        # node atual até o pai pelo melhor caminho (menor custo)
+        f.bf_pai = p
+def bellman_ford(vertices, pai):
+    # reset node configs
+    for v in vertices:
+        v.bf_distancia = float('inf')
+        v.bf_pai = None
+
+    pai.bf_distancia = 0
+    for v in vertices[:-1]:
+        for ar in v.conexoes:
+            relax(v, ar.dst)
+    for v in vertices:
+        print(f'{v.id} -> {v.bf_distancia if v.bf_distancia != float("inf") else f"SEM CAMINHO PARTINDO DE {pai.id}"}')
+
+def dijkstra(vertices, pai, dst):
+    # reset node configs
+    resetVisitedNodes(vertices)
+    for v in vertices:
+        v.dj_distancia = float('inf')
+        v.dj_pai = None
+
+
+    print(f'\nComecou em {pai.id}')
+    pai.dj_distancia = 0
+    q = []
+    q.append(pai)
+    while q != []:
+        pai = q.pop(0)
+        for ar in pai.conexoes:
+            if ar.dst == dst:
+                print(f'\nChegou em {ar.dst.id}')
+                ar.dst.dj_distancia = pai.dj_distancia + ar.w
+                ar.dst.dj_pai = pai
+                
+                r = ar.dst.dj_pai
+                print(f'{ar.dst.id} <- ', end='')
+                while r != None:
+                    print(f'{r.id} <- ', end='')
+                    r = r.dj_pai
+                print(f'\n com distancia de {ar.dst.dj_distancia}')
+                return ar.dst
+
+            q.append(ar.dst)
+            if pai.dj_distancia + ar.w < ar.dst.dj_distancia:
+                ar.dst.dj_distancia = pai.dj_distancia + ar.w
+                ar.dst.dj_pai = pai
+    
+
+def copilot_floyd_warshall(vertices):
+    for k in vertices:
+        for i in vertices:
+            for j in vertices:
+                if i.dj_distancia + j.dj_distancia < k.dj_distancia + j.dj_distancia:
+                    k.dj_distancia = i.dj_distancia + j.dj_distancia
+                    k.dj_pai = j
+
+
+def floyd_warshall(vertices):
+    pass
+
+
 def main():
     # vertices ficao guardados como ponteiros dentro de 'vertices'
     vertices = []
 
     # arestas determina quais conexoes entre os nos sao feitas
-    arestas = [(1,2, 1), (1,4, 2), (2,3, 1), (2,4, 3), (3,7, 1), (4,8, 2), (5,7, 1), (5,6, 2), (8,7, 1)]
+    arestas = [(1,2, 2), (1,4, 2), (2,3, -3), (2,4, -1), (3,7, -2), (4,8, 2), (5,7, 1), (5,6, 2), (8,7, 4)]
     
     # * = (nao tem conexao para o resto do grafo entao eles entram depois)
     # bfs -> 1 2 4 3 8 7 * 5 6
@@ -119,7 +200,9 @@ def main():
 
     bfs(vertices, vertices[0])
     dfs(vertices, vertices[0])
-
+    bellman_ford(vertices, vertices[0])
+    dijkstra(vertices, vertices[0], vertices[6])
+    
 
 if __name__ == "__main__":
     main()
