f27e8fbff39ddb60fad663a0914b6e564725ce41
Felipe Chatalov <73392575+felipechatalov@users.noreply.github.com>
2022-04-16 01:46:07 -0300

    Add files via upload

--- grafo.py
+++ grafo.py
@@ -12,6 +12,10 @@ class Vertice():
         self.dj_pai = None
         self.dj_distancia = float('inf')
 
+        # prim 
+        self.pm_pai = None
+        self.pm_distancia = float('inf')
+
     def __str__(self):
         return f'id: {self.id}'
 
@@ -22,7 +26,12 @@ class Vertice():
         for aresta in self.conexoes:
             if aresta.dst == dst:
                 return aresta
-
+    def getArestaMenorCusto(self):
+        menor = self.conexoes[0]
+        for aresta in self.conexoes:
+            if aresta.weight < menor.weight:
+                menor = aresta
+        return menor
 
 class Aresta():
     def __init__(self, dst, w):
@@ -30,15 +39,10 @@ class Aresta():
         self.w = w
 
 
-def resetVisitedNodes(g):
+def bfs(g, pai):
+    print("BFS")
     for v in g:
         v.visited = False
-
-
-
-
-def bfs(g, pai):
-    resetVisitedNodes(g)
 
     q = []
     seq = []
@@ -46,7 +50,7 @@ def bfs(g, pai):
     q.append(pai)
     seq.append(pai)
 
-    print(f'\nComecou em {pai.id}')
+    print(f'Comecou em {pai.id}')
 
     while q != []:
         pai = q.pop(0)
@@ -67,11 +71,13 @@ def bfs(g, pai):
     print(f'\nSequencia de visita em lista: ')
     for v in seq:
         print(v.id, end=" ") 
-    print()
+    print('\n')
     return seq
 
 def dfs(g, pai):
-    resetVisitedNodes(g)
+    print("DFS")
+    for v in g:
+        v.visited = False
     
     q = []
     seq = []
@@ -79,7 +85,7 @@ def dfs(g, pai):
     q.append(pai)
     seq.append(pai)
 
-    print(f'\nComecou em {pai.id}')
+    print(f'Comecou em {pai.id}')
 
     while q != []:
         pai = q.pop(0)
@@ -100,9 +106,8 @@ def dfs(g, pai):
     print(f'\nSequencia de visita em lista: ')
     for v in seq:
         print(v.id, end=" ") 
-    print()
+    print('\n')
     return seq
-
 
 def relax(p, f):
     # print(f'\n relaxando {p.id} -> {f.id}')
@@ -112,6 +117,7 @@ def relax(p, f):
         # node atual até o pai pelo melhor caminho (menor custo)
         f.bf_pai = p
 def bellman_ford(vertices, pai):
+    print("Bellman-Ford")
     # reset node configs
     for v in vertices:
         v.bf_distancia = float('inf')
@@ -123,68 +129,137 @@ def bellman_ford(vertices, pai):
             relax(v, ar.dst)
     for v in vertices:
         print(f'{v.id} -> {v.bf_distancia if v.bf_distancia != float("inf") else f"SEM CAMINHO PARTINDO DE {pai.id}"}')
-
-def dijkstra(vertices, pai, dst):
+    print('\n')
+def dijkstra(vertices, pai):
+    print("Dijkstra")
     # reset node configs
-    resetVisitedNodes(vertices)
+
     for v in vertices:
         v.dj_distancia = float('inf')
         v.dj_pai = None
+        v.visited = False
 
-
-    print(f'\nComecou em {pai.id}')
+    print(f'Comecou em {pai.id}')
     pai.dj_distancia = 0
     q = []
     q.append(pai)
     while q != []:
-        pai = q.pop(0)
-        for ar in pai.conexoes:
-            if ar.dst == dst:
-                print(f'\nChegou em {ar.dst.id}')
-                ar.dst.dj_distancia = pai.dj_distancia + ar.w
-                ar.dst.dj_pai = pai
-                
-                r = ar.dst.dj_pai
-                print(f'{ar.dst.id} <- ', end='')
-                while r != None:
-                    print(f'{r.id} <- ', end='')
-                    r = r.dj_pai
-                print(f'\n com distancia de {ar.dst.dj_distancia}')
-                return ar.dst
-
-            q.append(ar.dst)
-            if pai.dj_distancia + ar.w < ar.dst.dj_distancia:
-                ar.dst.dj_distancia = pai.dj_distancia + ar.w
-                ar.dst.dj_pai = pai
+        p = q.pop(0)
+        p.visited = True
+        for ar in p.conexoes:
+            if ar.dst.visited == False:
+                q.append(ar.dst)
+                ar.dst.visited = True
+            if p.dj_distancia + ar.w < ar.dst.dj_distancia:
+                ar.dst.dj_distancia = p.dj_distancia + ar.w
+                ar.dst.dj_pai = p
     
-
-def copilot_floyd_warshall(vertices):
-    for k in vertices:
-        for i in vertices:
-            for j in vertices:
-                if i.dj_distancia + j.dj_distancia < k.dj_distancia + j.dj_distancia:
-                    k.dj_distancia = i.dj_distancia + j.dj_distancia
-                    k.dj_pai = j
-
-
+    for v in vertices:
+        print(f'{v.id} -> {v.dj_distancia if v.dj_distancia != float("inf") else f"SEM CAMINHO PARTINDO DE {pai.id}"}')
+    print()
+# nao funciona com grafo nao orientado com valores negativos
+# mas funciona com grafo orientado com valores negativos
 def floyd_warshall(vertices):
-    pass
+    print("Floyd-Warshall")
+    d = [[float('inf') for i in range(len(vertices))] for j in range(len(vertices))]
+    pred = [[float('inf') for i in range(len(vertices))] for j in range(len(vertices))]
+    for i in range(len(vertices)):
+        for j in range(len(vertices)):
+            if i == j:
+                d[i][j] = 0
+            elif vertices[i].getArestaFromVertice(vertices[j]):
+                d[i][j] = vertices[i].getArestaFromVertice(vertices[j]).w
+            pred[i][j] = i
+
+    # for i in range(len(vertices)):
+    #     for j in range(len(vertices)):
+    #         print(f'{d[i][j]}\t', end='')
+    #     print()
+
+    for i in range(len(vertices)):
+        for j in range(len(vertices)):
+            for k in range(len(vertices)):
+                if d[i][j] > d[i][k] + d[k][j]:
+                    d[i][j] = d[i][k] + d[k][j]
+                    pred[i][j] = pred[k][j]
+
+    for i in range(len(vertices)):
+        for j in range(len(vertices)):
+            print(f'{d[i][j]}\t', end='')
+        print()
+
+    print()
+    for i in range(len(vertices)):
+        for j in range(len(vertices)):
+            print(f'{pred[i][j]}\t', end='')
+        print()
+    print()
+    # pred funciona assim:
+    # caso vc queria saber o caminho de menor distancia do vertice 0 ao 6
+    # vc olha pred[0][6] e ve que o valor eh 2,
+    # entao vc olha o menor caminho de 0 a 2, ou seja, pred[0][2] e ve que o valor eh 1,
+    # entao vc olha pred[0][1] e ve que o valor eh 0, que eh o vertice que queremos comecar
+    # logo temos o menor caminho de 0 ate 6, 0->1->2->6
+    # neste caso, menor caminho == menor custo
+
+    return d, pred
+
+
+def prim(vertices, pai):
+    print("Prim")
+    for v in vertices:
+        v.pm_distancia = float('inf')
+        v.pm_pai = None
+        v.visited = False
+
+    v = len(vertices)
+    noEdge = 0
+    selected = [0]*v
+    selected[0] = True
+    
+    l = []
+
+    while noEdge < v-1:
+        minimum = float('inf')
+        x, y = 0, 0
+        for i in range(v):
+            if selected[i] == True:
+                for j in range(v):
+                    if not selected[j] and vertices[i].getArestaFromVertice(vertices[j]):
+                        if vertices[i].getArestaFromVertice(vertices[j]).w < minimum:
+                            minimum = vertices[i].getArestaFromVertice(vertices[j]).w
+                            x = i
+                            y = j
+
+
+        print(f'{vertices[x].id} -> {vertices[y].id}')
+        selected[y] = True
+        if vertices[x] not in l:
+            l.append(vertices[x])
+        if vertices[y] not in l:
+            l.append(vertices[y])
+        noEdge+=1
+    for i in range(len(l)):
+        print(f'{l[i].id} ', end='')
+    print()
+    return l
 
 
 def main():
     # vertices ficao guardados como ponteiros dentro de 'vertices'
     vertices = []
-
+    lenv = 8
     # arestas determina quais conexoes entre os nos sao feitas
-    arestas = [(1,2, 2), (1,4, 2), (2,3, -3), (2,4, -1), (3,7, -2), (4,8, 2), (5,7, 1), (5,6, 2), (8,7, 4)]
-    
-    # * = (nao tem conexao para o resto do grafo entao eles entram depois)
-    # bfs -> 1 2 4 3 8 7 * 5 6
-    # dfs -> 1 2 3 7 4 8 7 * 5 6
+    arestas = [(1,2, 2), (1,4, 2), 
+               (2,3, -3), (2,4, -1), 
+               (3,7, -2), 
+               (4,8, 2), (4,1, -1),
+               (5,7, 1), (5,6, 2),
+               (6,5, 2), 
+               (8,7, 4)]
+    # arestas = [(1,2, 1), (1,5, 1), (2,3, 2), (2,4, 2), (2,5, 2), (3,5, 3), (4,6, 4), (5,6, 3)]
 
-
-
-    for i in range(8):
+    for i in range(lenv):
         v = Vertice(i+1)
         vertices.append(v)
    
@@ -195,14 +270,22 @@ def main():
     #     pv.conexoes.append(uv)
     #     uv.conexoes.append(pv)
 
+    # orientado
     for src, dst, w in arestas:
         vertices[src-1].addAresta(vertices[dst-1], w)
 
+    # nao orientado
+    # for src, dst, w, in arestas:
+    #     vertices[src-1].addAresta(vertices[dst-1], w)
+    #     vertices[dst-1].addAresta(vertices[src-1], w)
+
+    print(vertices[0].id)
     bfs(vertices, vertices[0])
     dfs(vertices, vertices[0])
     bellman_ford(vertices, vertices[0])
-    dijkstra(vertices, vertices[0], vertices[6])
-    
+    dijkstra(vertices, vertices[0])
+    floyd_warshall(vertices)
+    prim(vertices, vertices[0])
 
 if __name__ == "__main__":
     main()
